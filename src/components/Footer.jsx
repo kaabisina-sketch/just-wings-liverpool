77bec20cad334e7150f36c03f27013bec3bf7840
sina kaabi <kaabisina@gmail.com>
2026-04-19 18:17:21 +0000

    File changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,11 +43,11 @@ export default function Footer() {
 
         {/* Social */}
         <div className="flex justify-center gap-4 mb-6">
-          <a href="#" className="text-black hover:text-gray-500 transition-colors">
+          <a href="https://www.instagram.com/justwingsuk/?hl=en" target="_blank" rel="noopener noreferrer" className="text-black hover:text-gray-500 transition-colors">
             <Instagram size={22} />
           </a>
           {/* TikTok icon (custom SVG as lucide doesn't have it) */}
-          <a href="#" className="text-black hover:text-gray-500 transition-colors">
+          <a href="https://www.tiktok.com/@justwings19" target="_blank" rel="noopener noreferrer" className="text-black hover:text-gray-500 transition-colors">
             <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
               <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.34 6.34 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.18 8.18 0 0 0 4.78 1.52V6.74a4.85 4.85 0 0 1-1.01-.05z" />
             </svg>
